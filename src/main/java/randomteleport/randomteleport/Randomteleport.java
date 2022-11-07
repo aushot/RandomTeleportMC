c71b6f4e2a51aa1f5772d4b405e8f8bd0510530d
aushot <marcomaraglino5@gmail.com>
2022-11-08 00:00:14 +0100

    metrics added

--- src/main/java/randomteleport/randomteleport/Randomteleport.java
+++ src/main/java/randomteleport/randomteleport/Randomteleport.java
@@ -3,11 +3,15 @@ package randomteleport.randomteleport;
 import org.bukkit.plugin.java.JavaPlugin;
 import randomteleport.randomteleport.Listeners.OnSignChanged;
 import randomteleport.randomteleport.Listeners.TeleportSignEvent;
+import randomteleport.randomteleport.Metrics.Metrics;
 
 public final class Randomteleport extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        int pluginId = 16830; // <-- Replace with the id of your plugin!
+        Metrics metrics = new Metrics(this, pluginId);
 
         getServer().getPluginManager().registerEvents(new OnSignChanged(), this);
         getServer().getPluginManager().registerEvents(new TeleportSignEvent(), this);
